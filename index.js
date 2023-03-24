f5dd8b6c60c71b723c100fb7a4f1d5030772b857
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-03-24 08:28:09 -0400

    UserS-9 completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const advice = document.querySelector(".text");
 const anotherAdvice = document.querySelector(".another-quote");
 const twitterAnchor = document.querySelector(".twitter");
+const main = document.querySelector("main");
 // Variable to store the advice sentence
 let adviceInfo;
 
@@ -26,9 +27,31 @@ const shareOnTwitter = () => {
   window.open(twitterUrl);
 };
 
-// Call the change text function when button is clicked
+// Function to randomize the background color
+const getRandomColor = () => {
+  // String with all possible letters in a hexa color code
+  const letters = "0123456789ABCDEF";
+  // All colors begin with a #
+  let color = "#";
+  // Loop 6 times
+  for (let i = 0; i < 6; i++) {
+    // Randomize the letter in each index
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
+
+// Function to change the background color of main
+const changeBackgroundColor = () => {
+  const randomColor = getRandomColor();
+  main.style.backgroundColor = randomColor;
+};
+
+// Call the change text function and randomize color when button is clicked
 anotherAdvice.addEventListener("click", () => {
   changeText();
+  changeBackgroundColor();
 });
 
 // Call the shareOnTwitter function when twitter anchor tag is clicked
