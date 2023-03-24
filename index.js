e8ac39e76ee1cb60bc907bb640ce2a1f51d7ea90
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-03-24 07:29:27 -0400

    Added more comments, UserS-6 completed

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const fetchData = async () => {
 
 // Function to change the text of the advice
 const changeText = async () => {
+  // Change value of advice to the return of the fetchData function, which is the object given by the API
   adviceInfo = await fetchData();
+  // Get the sentence from the object
   advice.textContent = adviceInfo.slip.advice;
 };
 
